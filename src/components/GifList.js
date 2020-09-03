b75c0275ce80307641c6165278dbeef887206952
Mely07 <damely.tineo07@gmail.com>
2020-09-03 00:21:06 -0400

    Done.

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -1,13 +1,17 @@
 import React from 'react'
 
 function GifList(props) {
-    let gifArray = props.gifs.map((gifObj) => <li><img src={gifObj.images.original.url} /></li>)
-    console.log("gif array: ", gifArray)
+    
     return (
         <ul>
-            {gifArray}
+            {props.gifs.map((gifObj) => <li><img src={gifObj.images.original.url} /></li>)}
         </ul>
     )
 }
 
 export default GifList
+
+
+{/* <GifList /> is a presentational component. 
+It receives data from its props and renders html given the input data. 
+It can render a top level <ul> with each gif as an <li>. */}
--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -8,6 +8,19 @@ class GifListContainer extends Component {
         gifs: []
     }
 
+    submitHandler = (searchTerm) => {
+        this.fetchGifs(searchTerm)
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <GifSearch submitHandler={this.submitHandler} />
+                <GifList gifs={this.state.gifs} /> 
+            </React.Fragment>
+        )
+    }
+    
     componentDidMount() {
         this.fetchGifs()
     }
@@ -20,21 +33,13 @@ class GifListContainer extends Component {
     fetchGifs = (term = "dolphins") => {
         fetch(`https://api.giphy.com/v1/gifs/search?q=${term}&api_key=dc6zaTOxFJmzC&rating=g&limit=10`)
             .then(resp => resp.json())
-            .then(data => this.setState({ gifs: data.data }))
-    }
-
-    submitHandler = (searchTerm) => {
-        this.fetchGifs(searchTerm)
-    }
-
-    render() {
-        return (
-            <React.Fragment>
-                <GifSearch submitHandler={this.submitHandler} />
-                <GifList gifs={this.state.gifs} />
-            </React.Fragment>
-        )
+            .then(data=> this.setState({ gifs: data.data}))
     }
 }
 
 export default GifListContainer
+
+{/* <GifListContainer /> will be responsible for fetching the data from the giphy API, 
+storing the first 3 gifs from the response in its component state, and passing that data down to its child, 
+the <GifList> component, as a prop. */}
+
